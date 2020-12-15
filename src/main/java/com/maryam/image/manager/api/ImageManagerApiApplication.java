638914d8dcbeecc1bb2cc48d916a6294a8d89a0e
Maryam Karimi <mkarimif@uwo.ca>
2020-12-15 12:23:24 -0500

    The main class for the spring boot application

---
+++ src/main/java/com/maryam/image/manager/api/ImageManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.maryam.image.manager.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageManagerApiApplication.class, args);
+	}
+
+}
